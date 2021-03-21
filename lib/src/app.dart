c3522dbb97ff685be0340c63fe6e4d0e53f6e570
4eug <debraheug@gmail.com>
2021-03-21 18:04:40 +0000

    First Commit

---
+++ lib/src/app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:login_auth_firebase/src/screens/login.dart';
+
+class App extends StatelessWidget {
+  const App({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Login App',
+      theme: ThemeData(accentColor: Colors.orange, primarySwatch: Colors.blue),
+      home: LoginScreen(),
+    );
+  }
+}
---
+++ lib/src/screens/home.dart
@@ -0,0 +1,24 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:login_auth_firebase/src/screens/login.dart';
+
+class HomeScreen extends StatelessWidget {
+  final auth = FirebaseAuth.instance;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        // ignore: deprecated_member_use
+        child: FlatButton(
+          child: Text('Log Out'),
+          onPressed: () {
+            auth.signOut();
+            Navigator.of(context).pushReplacement(
+                MaterialPageRoute(builder: (context) => LoginScreen()));
+          },
+        ),
+      ),
+    );
+  }
+}
